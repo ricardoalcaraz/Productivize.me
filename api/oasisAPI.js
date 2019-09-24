b4765e968c1b55a1a43af917ff18e5f9f47e743a
Miguel Campos <amodestduck@gmail.com>
2019-09-23 22:26:18 -0700

    add node server notes,

--- api/oasisAPI.js
+++ api/oasisAPI.js
@@ -2,22 +2,27 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port = process.argv.slice(2)[0];
+const port = process.argv.slice(2)[0]; //use docker args.
 const app = express();
+// TODO: Add repository.
+
+/* MIDDLEWARE */
 app.use(bodyParser.json());
 app.use(cors());
 
 //TODO: Figure out how to run up with all its needed dependencies
-//TODO: Figure how to get rid of cors
-//TODO: Create an endpoint that will query the database
+//TODO: Figure how to get rid of cors <= just delete it bro.
+//TODO: Create an endpoint that will query the database <= server.js has it
+/* API */
 app.get('/habits', (req, res) => {
-    console.log('Returning habit list');
-    res.send("Hello");
+  console.log('Returning habit list');
+  res.send("Hello");
 });
 
 app.get('/test', (req, res) => {
-    console.log('Returning random number');
-    res.send(Math.random().toString());
+  console.log('Returning random number');
+  res.send(Math.random().toString());
 });
 
-app.listen(port);
+/* START */
+app.listen(port, () => console.log(`App listening on port: ${port}`));
