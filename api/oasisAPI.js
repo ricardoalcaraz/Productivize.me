fae7d9da799f3320d0c9ef1b7c507dfefea7b087
Ricardo Alcaraz <ralcarazerhs@gmail.com>
2019-09-25 00:35:23 -0700

    Removed cors requirement and set up nginx to serve static content and to reverse proxy api requests

--- api/oasisAPI.js
+++ api/oasisAPI.js
@@ -2,7 +2,6 @@ const { Pool } = require('pg');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
 const port = process.env.PORT;
 const databaseURL = process.env.DATABASE_URL;
@@ -13,20 +12,19 @@ const pool = new Pool({
 
 /* MIDDLEWARE */
 app.use(bodyParser.json());
-app.use(cors());
 
 /* API */
-app.get('/habits', (req, res) => {
+app.get('/api/habits', (req, res) => {
   console.log('Returning habit list');
   res.send("Hello");
 });
 
-app.get('/test', (req, res) => {
+app.get('/api/test', (req, res) => {
   console.log('Returning random number');
   res.send(Math.random().toString());
 });
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   pool.query('SELECT NOW()', (e, r) => {
     console.log('Successfully queried the database')
     res.send(!!e ? e : r) //send error if exists.
