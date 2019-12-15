318e2c6aa53fb9bbf8fa2ed92c5d18f27c776872
ricardoalcaraz <ralcarazerhs@gmail.com>
2019-12-15 14:32:30 -0800

    [/] Remade the project to use proper name for release builds (#72)

--- client/android/app/src/main/java/com/productivize/MainApplication.java
+++ client/android/app/src/main/java/com/productivize/MainApplication.java
@@ -1,4 +1,4 @@
-package com.phoneapp;
+package com.productivize;
 
 import android.app.Application;
 import android.content.Context;
--- client/ios/ProductivizeTests/ProductivizeTests.m
+++ client/ios/ProductivizeTests/ProductivizeTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface phoneAppTests : XCTestCase
+@interface ProductivizeTests : XCTestCase
 
 @end
 
-@implementation phoneAppTests
+@implementation ProductivizeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
